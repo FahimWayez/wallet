992242904004575a0ae7bc941076fd9ae56907a9
Fahim Wayez <fahimwayez1@gmail.com>
2024-02-26 22:43:14 +0600

    register api created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import * as bip39 from 'bip39';
+import * as elliptic from 'elliptic';
+import { UserService } from './user.service';
+import { register } from 'module';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('register')
+  async register(): Promise<any> {
+    const mnemonic = bip39.generateMnemonic();
+    const ec = new elliptic.ec('secp256k1');
+    const keyPair = ec.genKeyPair();
+    const publicKey = keyPair.getPublic('hex');
+    const privateKey = keyPair.getPrivate('hex');
+
+    const user = await this.userService.create({
+      passphrase: mnemonic,
+      publicKey,
+      privateKey,
+    });
+
+    return {
+      mnemonic,
+      publicKey: user.publicKey,
+    };
+  }
+}
